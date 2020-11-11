0524e7e67d3f08e09d3322f5a601b05a986126d5
horacio <horacio.jose@pbh.gov.br>
2020-11-11 16:02:45 -0300

    Implementando pessoa

---
+++ src/main/java/br/com/jway/bean/PessoaBean.java
@@ -0,0 +1,133 @@
+package br.com.jway.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import br.com.jway.geraesooh.model.Pessoa;
+import br.com.jway.geraesooh.service.PessoaService;
+import br.com.jway.util.FacesUtils;
+
+@ManagedBean
+@ViewScoped
+public class PessoaBean extends SpringBeanAutowiringSupport implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	protected static final Log log = LogFactory.getLog(PessoaBean.class);
+
+	@Inject
+	private PessoaService service;
+	
+	private String state;
+	private List<Pessoa> items;
+	private Pessoa item;
+	private Pessoa itemFilter;
+	
+	
+	public PessoaBean() {
+		log.info("Bean constructor called.");
+		itemFilter = new Pessoa();
+		limpaPesquisa();
+			}
+	
+	
+	@PostConstruct
+	private void postConstruct() {
+		log.info("Bean @PostConstruct called.");
+		state = "READ";
+		items = service.list();
+	}
+	
+	public void clearItems() {
+		if (items != null) {
+			items.clear();
+		}
+	}
+	
+	public void clearItem() {
+		try {
+			// Instantiating via reflection was used here for generic purposes
+			item = Pessoa.class.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+				FacesUtils.addI18nError("generic.bean.unableToCleanViewData");
+		}
+	}
+	
+	public void create() {
+		service.create(item);
+		limpaPesquisa();
+		items = service.list();
+		item = null;
+	}
+	
+	public void update() {
+		service.update(item);
+		limpaPesquisa();
+		items = service.list();
+		item = null;
+	}
+	
+	public void delete() {
+		service.delete(item.getId());
+		limpaPesquisa();
+		items = service.list();
+		item = null;
+	}
+	
+	public void pesquisa(){
+		items = service.pesquisa(item);
+	}
+	
+	public void limpaPesquisa(){
+	
+	}
+	
+	@PreDestroy
+	private void preDestroy() {
+		log.info("Bean @PreDestroy called.");
+	}
+	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public List<Pessoa> getItems() {
+		return items;
+	}
+	
+	public void setItems(List<Pessoa> items) {
+		this.items = items;
+	}
+	
+	public Pessoa getItem() {
+		return item;
+	}
+	
+	public void setItem(Pessoa item) {
+		this.item = item;
+	}
+	public Pessoa getItemFilter() {
+		return itemFilter;
+	}
+	
+	public void setItemFilter(Pessoa itemFilter) {
+		this.itemFilter = itemFilter;
+	}
+
+
+
+}
